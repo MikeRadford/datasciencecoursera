00e2a970785eb1e46f48df735058e8cf33ba57d1
Mike Radford <michael.radford@hotmail.co.uk>
2014-12-03 10:00:14 +0000

    Create plot 2 in assignment

---
+++ plot2.R
@@ -0,0 +1,12 @@
+# load data into dat frame, reduce dataset, resolve data frame, plot graph and export graphic
+setClass("myDate")
+setAs("character", "myDate", function(from) as.Date(from, format="%d/%m/%Y") )
+power <- read.table("household_power_consumption.txt", sep=";", header=TRUE, colClasses = c("myDate","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),na.strings=c("?"))
+summary(power)
+str(power)
+power2 <- subset(power, Date >= "2007-02-01" & Date <= "2007-02-02")
+temp <- paste(power2$Date,power2$Time)
+power2$DateTime <- strptime(temp, "%Y-%m-%d %H:%M:%S")
+hist(power2$Global_active_power, col="red", xlab = "Global Active Power (kilowatts)",main = "Global Active Power")
+dev.copy(png,file="plot2.png")
+dev.off()
